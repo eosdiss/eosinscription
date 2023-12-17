f8d8499fae4fa7bdfd032ac6fb7a5c94d4236d00
TwilightLogic <twilightlogic@gmail.com>
2023-12-17 21:53:27 +0800

    feat(app): complete UI creation

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,8 @@ import ChainForm from "./_components/chain-form";
 import InscriptionForm from "./_components/inscription-form";
 import ReceivedAddress from "./_components/received-address";
 import MintAmount from "./_components/mint-amount";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 // const privateKeys = [];
 // const signatureProvider = new JsSignatureProvider(privateKeys);
@@ -54,33 +56,54 @@ const App = () => {
     <>
       <WagmiConfig config={wagmiConfig}>
         <RainbowKitProvider chains={chains}>
-          {/* navbar */}
-          <nav className="flex items-center justify-between flex-wrap bg-teal-500 px-12 py-6">
-            <h2 className="text-3xl font-bold text-white">EOSInscription 🌼</h2>
-            <ConnectButton />
-          </nav>
+          <div className="flex flex-col justify-between h-screen">
+            {/* navbar */}
+            <nav className="flex items-center justify-between bg-teal-500 px-12 py-4">
+              <h2 className="text-3xl font-bold text-white">
+                EOSInscription 🌼
+              </h2>
+              <ConnectButton />
+            </nav>
 
-          {/* hero */}
-          <div className="flex flex-col items-center justify-center mt-12 mb-6 gap-3">
-            <h3 className="text-neutral-950 text-4xl font-bold tracking-tight">
-              EOS Inscription Tool
-            </h3>
-            <p className="text-muted-foreground">
-              Mint inscriptions on EOS chain.
-            </p>
-          </div>
-
-          {/* chains */}
-          <main className="flex min-h-screen flex-col items-start justify-start px-12">
-            <div className="flex flex-col items-center justify-center gap-3">
-              <div className="flex flex-col gap-6">
-                <ChainForm />
-                <InscriptionForm />
-                <ReceivedAddress />
-                <MintAmount />
-              </div>
+            {/* hero */}
+            <div className="flex flex-col items-center justify-center mt-12 mb-6 gap-3">
+              <h3 className="text-neutral-950 text-4xl font-bold tracking-tight">
+                EOS Inscription Tool
+              </h3>
+              <p className="text-muted-foreground">
+                Mint inscriptions on EOS chain.
+              </p>
             </div>
-          </main>
+
+            {/* chains */}
+            <main className="flex h-full flex-col items-start justify-start px-12">
+              <div className="flex flex-col items-center justify-center gap-3">
+                <div className="flex flex-col gap-4">
+                  <ChainForm />
+                  <InscriptionForm />
+                  <ReceivedAddress />
+                  <MintAmount />
+                  <Button
+                    size="lg"
+                    type="submit"
+                    className="bg-teal-500 hover:bg-teal-600 transition mt-4"
+                  >
+                    <span className="">Confirm</span>
+                  </Button>
+                </div>
+              </div>
+            </main>
+
+            {/* footer */}
+            <footer className="p-4">
+              <Link
+                href="https://github.com/eosdiss/eosinscription"
+                className="flex items-center justify-center text-sm text-gray-500 hover:underline transition"
+              >
+                Made by ❤️. EOSInscription team 🛠️.
+              </Link>
+            </footer>
+          </div>
         </RainbowKitProvider>
       </WagmiConfig>
     </>
