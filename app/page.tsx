2d7e9616103e0756386d6b6d986166dcd0c31362
TwilightLogic <twilightlogic@gmail.com>
2023-12-17 15:11:41 +0800

    feat(app): ui init

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,40 @@
-import Image from 'next/image'
-import { Api, JsonRpc } from 'eosjs';
-import { JsSignatureProvider } from 'eosjs/dist/eosjs-jssig';  // development only
+import Image from "next/image";
+import { Api, JsonRpc } from "eosjs";
+import { JsSignatureProvider } from "eosjs/dist/eosjs-jssig"; // development only
 
-import { createPublicClient, http } from 'viem'
-import { mainnet } from 'viem/chains'
+import { createPublicClient, http } from "viem";
+import { mainnet } from "viem/chains";
+import { ChainsForm } from "./_components/chains-form";
 
 export default async function Home() {
-
   const client = createPublicClient({
     chain: mainnet,
     transport: http(),
-  })
+  });
 
-  const rpc = new JsonRpc('http://eos.newdex.one'); //required to read blockchain state
+  const rpc = new JsonRpc("http://eos.newdex.one"); //required to read blockchain state
 
   // const privateKeys = [];
   // const signatureProvider = new JsSignatureProvider(privateKeys);
   // const api = new Api({ rpc, signatureProvider }); //required to submit transactions
-  // (async () => { 
+  // (async () => {
   //   await rpc.get_block(1) //get the first block
   // })();
 
-  const blockNumber = await client.getBlockNumber()
+  const blockNumber = await client.getBlockNumber();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div>{blockNumber.toString()}</div>
-    </main>
-  )
+    <>
+      {/* navbar */}
+      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+        <h2 className="text-2xl font-bold text-teal-50">eosInscription</h2>
+      </nav>
 
-  
+      {/* chains */}
+      <main className="flex min-h-screen flex-col items-start justify-start p-12">
+        <ChainsForm />
+        <div>{blockNumber.toString()}</div>
+      </main>
+    </>
+  );
 }
